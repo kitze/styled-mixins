168138c072422137baa36cf9dacbf658bf532369
Kristijan Ristovski <kristijan.mkd@gmail.com>
2019-08-04 11:55:23 +0200

    add mixins

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,54 @@
-import * as React from 'react';
+import { darken } from 'polished';
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
+export const marginVertical = (marginTop, marginBottom = marginTop) => ({
+  marginTop,
+  marginBottom,
+});
+
+export const screenSize = {
+  width: '100vw',
+  height: '100vh',
 };
+
+export const absolute = {
+  position: 'absolute',
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+};
+
+export const ellipsis = {
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+};
+
+export const fixedHeight = height => ({
+  height,
+  maxHeight: height,
+  minHeight: height,
+});
+
+export const fixedWidth = width => ({
+  width,
+  maxWidth: width,
+  minWidth: width,
+});
+
+export const whiteish = (opacity = 1) => `rgba(255,255,255, ${opacity})`;
+
+export const hoverAndFocus = style => ({
+  '&:hover': style,
+  '&:focus': style,
+});
+
+export const hoverAndActiveBackgroundColor = color => ({
+  backgroundColor: color,
+  '&:hover': {
+    backgroundColor: darken(0.03, color),
+  },
+  '&:active': {
+    backgroundColor: darken(0.04, color),
+  },
+});
